e9743b190074b6e307071952b93d80ea3e7beee7
A-nobody <13622095721@163.com>
2019-01-07 19:21:56 +0800

    用icon写footer

---
+++ router/src/components/home/components/HearderW.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="header_w">
+        <p>
+            <!-- <router-link :to="##">关注</router-link>
+            <router-link :to="##">推荐</router-link> -->
+            <!-- 关注，推荐 -->
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+    #header_w{
+        width: 100%;
+        height: .88rem;
+        background:pink;
+        position: fixed;
+        top:0;
+        left:0;
+    }
+</style>
+
