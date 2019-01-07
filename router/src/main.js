7628cc36a9d67da79a7c49231ee5529490363763
t-sun-j <15321580779@163.com>
2019-01-07 23:13:41 +0800

    1.7第二次提交

--- router/src/main.js
+++ router/src/main.js
@@ -3,6 +3,8 @@ import App from './App'
 import router from './router'
 import "./common/css/reset.css"
 import "./common/js/flexble"
+import oBserver from "./common/js/observer";
+Vue.prototype.Observer = oBserver;
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
