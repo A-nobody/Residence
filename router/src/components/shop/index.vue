5c80a76e2ac219839ad75c6a2673d4203d70d58d
guanpingping <45351979+guanpingping@users.noreply.github.com>
2019-01-08 09:21:03 +0800

    Update index.vue

--- router/src/components/shop/index.vue
+++ router/src/components/shop/index.vue
@@ -5,12 +5,8 @@
 </template>
 
 <script>
-// import Shophome from "./components/shophome.vue";
-// import goods_details_j from './components/goods_details_j'
 export default {
     components:{
-        // Shophome,
-    //    "goods-details-j-com":goods_details_j
     }
 }
 </script>
