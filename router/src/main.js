49d89add60dc2e841f56e5987e3b6ede1e1cb5e0
Your Name <you@example.com>
2019-01-09 09:34:42 +0800

    修改冲突

--- router/src/main.js
+++ router/src/main.js
@@ -5,15 +5,9 @@ import "./common/css/reset.css"
 import "./common/css/iconfont/iconfont.css"
 import "./common/js/flexble"
 import store from "./store";
-<<<<<<< HEAD
 
-import Observer from "@/common/js/observer.js";
-Vue.prototype.Observer = Observer;
-
-=======
 import observer from './common/js/observer'
 import './common/css/animate.min.css'
->>>>>>> master
 Vue.config.productionTip = false
 
 Vue.prototype.observer = observer
