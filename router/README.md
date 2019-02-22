bb1188cfb7c81ef8011a4ef1856dfe938066d051
DearBall <17513722019@163.com>
2019-02-22 17:05:43 +0800

    hello

--- router/README.md
+++ router/README.md
@@ -28,4 +28,6 @@ vuex
 qs
 路由懒加载
 sass 安装node-sass sass-loader
+图片懒加载 Lazy load
+
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- router/config/index.js
+++ router/config/index.js
@@ -6,21 +6,46 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
+    //https://www.easy-mock.com/mock/5c3481b05ee07b0e226214fa/example
+    //http://39.96.75.124:8080/HuiJu-1.0-SNAPSHOT/login?userName=user&userPassword=123456
     // Paths
+    //https://www.easy-mock.com/mock/5c34910a571e3a78765765f9/example/detail_img_article
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      // "/apiw": {
+      //   target: "http://10.9.69.245",
+      //   changeOrigin: true,
+      //   pathRewrite: {
+      //     "^/apiw": ""
+      //   },
+      // },
+      "/apin": {
+        target: "https://www.easy-mock.com",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/apin": ""
+        }
+      },
+    //https://www.easy-mock.com/mock/5c36e81c96e17359c184e2f8/huiju/shop/viewphoto
+    	"/api":{
+    		target:"https://www.easy-mock.com",
+    		changeOrigin:true,
+    		pathRewrite:{
+    			"^/api":""
+    		}
+    	}
+    },
 
     // Various Dev Server settings
-    host: '192.168.155.1', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    
+
     /**
      * Source Maps
      */
